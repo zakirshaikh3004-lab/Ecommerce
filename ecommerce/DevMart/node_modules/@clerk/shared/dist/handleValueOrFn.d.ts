849fe1d870c45a00d10c8cd679e8e353821c88a6
zakirshaikh3004-lab <zakirshaikh3004@gmail.com>
2025-09-28 10:04:16 +0530

    Delete ecommerce/DevMart/node_modules directory

--- ecommerce/DevMart/node_modules/@clerk/shared/dist/handleValueOrFn.d.ts
+++
@@ -1,8 +0,0 @@
-import { h as handleValueOrFn$1 } from './handleValueOrFn-D2uLOn6s.js';
-
-/**
- * @deprecated - Use `handleValueOrFn` from `@clerk/shared/utils` instead.
- */
-declare const handleValueOrFn: typeof handleValueOrFn$1;
-
-export { handleValueOrFn };
--- ecommerce/DevMart/node_modules/@clerk/shared/dist/web3.d.mts
+++
@@ -1,5 +0,0 @@
-import { Web3ProviderData } from '@clerk/types';
-
-declare const WEB3_PROVIDERS: Web3ProviderData[];
-
-export { WEB3_PROVIDERS };
